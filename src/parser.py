b033b61f3ba863e96373d56cbf82969554914551
Diana Bunina <d.bunina@hh.ru>
2018-04-19 13:32:35 +0300

    get top 10 words for each file

---
+++ src/parser.py
@@ -0,0 +1,45 @@
+import os
+from chardet.universaldetector import UniversalDetector
+import glob
+
+
+def detect_encoding(filename):
+    detector = UniversalDetector()
+    with open(filename, 'rb') as f:
+        for line in f:
+            detector.feed(line)
+            if detector.done:
+                break
+    detector.close()
+    return detector.result['encoding']
+
+
+def get_top_words_for_file(filename, encoding):
+    with open(filename, encoding=encoding) as f:
+        text = f.read()
+        words = text.split()
+        word_counter = {}
+        for word in words:
+            if len(word) > 6:
+                if word in word_counter:
+                    word_counter[word] += 1
+                else:
+                    word_counter[word] = 1
+        top_words = sorted(word_counter, key=word_counter.get, reverse=True)
+        return top_words[:10]
+
+
+def get_popular_words():
+    popular_words_by_article = dict()
+    os.chdir('./news/')
+    for filename in glob.glob('*.txt'):
+        encoding = detect_encoding(filename)
+        popular_words_by_article[filename] = get_top_words_for_file(filename, encoding)
+    return popular_words_by_article
+
+
+def main():
+    print(get_popular_words())
+
+
+main()
